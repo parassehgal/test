265d14d785a1b90af9016ca4d426fbf47bc1cf7a
Paras Sehgal <34476449+parassehgal@users.noreply.github.com>
2019-04-24 12:36:25 +0530

    Add files via upload

---
+++ tlogin.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+var fs = require("fs");
+const restService = express();
+
+restService.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+
+restService.use(bodyParser.json());
+restService.get("/login", function (req, res) {
+    res.write(fs.readFileSync('testlogin.ejs'));
+    res.write('<br/><br/><br/><br/> Body: ');
+    
+	res.write(JSON.stringify(req.body));
+	res.write('<br/><br/><br/><br/>Query: ');
+    
+	//console.log(req);
+	res.write(JSON.stringify(req.query));
+
+    res.end();
+});
+
+restService.post("/login", function (req, res) {
+    //console.log(req.body);
+    if (req.body.username == 'user' && req.body.password == 'password') {
+        res.redirect('https://www.google.com');
+    }
+    else {
+        res.write('<script>alert("Invalid user");</script>');
+        res.end();
+    }
+});
+
+restService.listen(process.env.PORT || 9999);
